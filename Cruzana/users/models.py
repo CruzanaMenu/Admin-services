e3bc6f0af141e0e83db6e0926e02099b628687f3
Sergio Alejandro Morales Cuesta <sam94c@gmail.com>
2022-09-01 10:29:32 -0500

    Commit para agregar models y app users

--- Cruzana/users/models.py
+++ Cruzana/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    class Gender(models.TextChoices):
+        male = "Masculino"
+        female = "Femenino"
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    telephone_number = models.DecimalField(max_digits=10, decimal_places=0)
+    gender = models.CharField(
+        max_length=10, 
+        choices = Gender.choices,
+        default=Gender.male)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.first_name
--- Cruzana/users/views.py
+++ Cruzana/users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request): 
-    return HttpResponse("Hello World - User moduls")
+    return HttpResponse("Información de los clientes")
+
+
+def detail(request, users_id):
+    return HttpResponse(f"Estas viendo al cliente número {users_id}")
